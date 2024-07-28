7603e06b739e343a1d393db45f2ad919dd2c391e
Alex Oliva <alexoliva87464@gmail.com>
2024-07-28 07:17:56 -0300

    feat: Add SaveFile use case and implement file saving functionality in ServerApp.run method

--- src/presentation/server-app.ts
+++ src/presentation/server-app.ts
@@ -1,16 +1,28 @@
 import { CreateTable } from '../domain/use-cases/create-table.use-case';
+import { SaveFile } from '../domain/use-cases/save-file.use-case';
 
 interface RunOptions {
   base: number;
   limit: number;
   showTable: boolean;
+  name: string;
+  dir: string;
 }
 
 export class ServerApp {
-  static run({ base, limit, showTable }: RunOptions) {
+  static run({ base, limit, showTable, name, dir }: RunOptions) {
     console.log('ServerApp running...');
     const table = new CreateTable().execute({ base, limit });
 
+    const fileCreated = new SaveFile().execute({
+      fileContent: table,
+      filePath: dir,
+      fileName: name,
+    });
+
     if (showTable) console.log(table);
+    fileCreated
+      ? console.log('File created!')
+      : console.log('Error creating file');
   }
 }
